1a735edb6beaaf0d610f7e12ccd2de613296edcf
Antônio Miguel <88382914+AntonioMiguel03@users.noreply.github.com>
2023-07-23 15:52:35 -0300

    Add files via upload

---
+++ PrjGelatina/JavaScript/program.js
@@ -0,0 +1,11 @@
+//Programacao dos botoes
+
+function irAnimacao() {
+    //window.open("https://www.google.com", "_blank");
+    window.location.href = "./animacao/animacao.html";
+}
+
+function irEvento() {
+    //window.open("https://www.google.com", "_blank");
+    window.location.href = "./evento/evento.html";
+}
